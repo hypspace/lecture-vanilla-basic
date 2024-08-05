17e570cc2775cf3a0a352922a828839fe266e553
Tae-hyeon Kim <hypspacecraft@gmail.com>
2024-08-05 12:59:56 +0900

    [1-vanilla] feat: resultview-2
    검색 결과 보임

--- 1-vanilla/src/controllers/MainController.js
+++ 1-vanilla/src/controllers/MainController.js
@@ -1,4 +1,7 @@
 import FormView from '../views/FormView.js'
+import ResultView from '../views/ResultView.js'
+
+import SearchModel from '../models/SearchModel.js'
 
 const tag = '[MainController]'
 
@@ -8,10 +11,21 @@ export default {
       .on('submit', e => e.preventDefault())
       .on('@submit', e => this.handleSubmitForm(e.detail.input))
       .on('@reset', () => this.handleClickReset())
+
+    ResultView.setup(document.querySelector('#search-result'))
+  },
+
+  search(query) {
+    SearchModel.list(query) //
+      .then(res => {
+        console.log(tag, 'search()', res)
+        ResultView.render(res)
+      })
   },
 
   handleSubmitForm(input) {
     console.log(tag, 'handleSubmitForm()', input)
+    this.search(input)
   },
 
   handleClickReset() {
---
+++ 1-vanilla/src/views/ResultView.js
@@ -0,0 +1,33 @@
+import View from './View.js'
+
+const tag = '[ResultView]'
+
+const ResultView = Object.create(View)
+
+ResultView.NO_RESULT = '검색 결과가 없습니다'
+
+ResultView.setup = function (el) {
+  this.init(el)
+}
+
+ResultView.render = function (data = []) {
+  this.el.innerHTML = data.length ? this.displayResults(data) : this.NO_RESULT
+}
+
+ResultView.displayResults = function (data) {
+  return (
+    data.reduce((html, item) => {
+      html += this.createHTMLString(item)
+      return html
+    }, '<ul>') + '</ul>'
+  )
+}
+
+ResultView.createHTMLString = function (item) {
+  return `<li>
+    <img src="${item.image}" alt="${item.name}"/>
+    <p>${item.name}</p>
+  </li>`
+}
+
+export default ResultView
