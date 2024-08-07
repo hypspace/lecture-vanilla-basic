cc120a652093d578cffb666ffb4203c64d10b6a5
Tae-hyeon Kim <hypspacecraft@gmail.com>
2024-08-07 13:56:21 +0900

    [2-vue] feat: keywordview-1
    추천 검색어 목록이 탭 아래 위치

--- 2-vue/src/app.js
+++ 2-vue/src/app.js
@@ -1,6 +1,8 @@
 import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.7.16/dist/vue.esm.browser.js'
 import SearchModel from './models/SearchModel.js'
+import KeywordModel from './models/KeywordModel.js'
 
+// TODO: 목록에서 검색어 클릭 시 선택된 검색어로 검색 결과 화면 이동
 new Vue({
   el: '#app',
   data: {
@@ -9,9 +11,11 @@ new Vue({
     hasSearched: false,
     tabs: ['추천 검색어', '최근 검색어'],
     selectedTab: '',
+    keywords: [],
   },
   created() {
     this.selectedTab = this.tabs[0]
+    this.fetchKeywords()
   },
   methods: {
     search(query) {
@@ -30,6 +34,13 @@ new Vue({
     },
     onChange(tabName) {
       this.selectedTab = tabName
+      if (tabName === '추천 검색어') {
+      }
+    },
+    fetchKeywords() {
+      KeywordModel.list().then(data => {
+        this.keywords = data
+      })
     },
   },
   watch: {
