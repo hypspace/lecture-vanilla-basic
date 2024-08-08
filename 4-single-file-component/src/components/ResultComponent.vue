35822a860d3b50451a689b177fd084da279e9dd3
Tae-hyeon Kim <hypspacecraft@gmail.com>
2024-08-08 14:13:25 +0900

    [4-single-file-component] refactor: modularize views with vue single file component
    Vue Single File Component를 활용하여 Views 로직 모듈화

---
+++ 4-single-file-component/src/components/ResultComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="search-result">
+    <div v-if="data.length">
+      <ul>
+        <li v-for="item in data">
+          <img v-bind:src="item.image" v-bind:alt="item.name" />
+          <p>{{ item.name }}</p>
+        </li>
+      </ul>
+    </div>
+    <div v-else>{{ query }} 검색 결과가 없습니다</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ResultComponent',
+  props: {
+    data: {
+      type: Array,
+      required: true,
+    },
+    query: String,
+  },
+}
+</script>
